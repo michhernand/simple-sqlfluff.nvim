1a884fa273c6bf46b8a603318ae9e93b6824e8d6
Michael Hernandez <mhernan88@gmail.com>
2025-05-14 00:13:57 -0500

    add autocmd options (#1)

---
+++ lua/simple-sqlfluff/settings.lua
@@ -0,0 +1,27 @@
+local M = {}
+
+M.options = {}
+
+M.defaults = {
+	autocommands = {
+		enabled = true,
+		events = {
+			"BufReadPost",
+			"InsertLeave",
+		},
+		extensions = {
+			"*.sql",
+		},
+	},
+}
+
+function M.resolve_opts(options)
+	M.options = vim.tbl_deep_extend(
+		"force",
+		{}, M.defaults,
+		options or {}
+	)
+end
+
+return M
+
